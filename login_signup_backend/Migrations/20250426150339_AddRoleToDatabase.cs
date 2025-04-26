6bbbd0d14b5f708902547a73be6f28f7573c779f
ismail vasip <vasip.343@gmail.com>
2025-04-26 18:05:08 +0300

    role configs are done.

---
+++ login_signup_backend/Migrations/20250426150339_AddRoleToDatabase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace login_signup.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "19646652-e735-4bdd-a2aa-9e15d658de9a", null, "Admin", "ADMIN" },
+                    { "e81189d7-80d6-4eb4-937d-d6edcb3a57ab", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "19646652-e735-4bdd-a2aa-9e15d658de9a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e81189d7-80d6-4eb4-937d-d6edcb3a57ab");
+        }
+    }
+}
--- login_signup_backend/repositories/RepositoryContext.cs
+++ login_signup_backend/repositories/RepositoryContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using login_signup_backend.models;
+using login_signup_backend.repositories.Config;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,9 @@ namespace login_signup_backend.repositories
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            //eğer birdenç çok IEntityTypeConfiguration ifaden varsa ortak bir assembly içerisinde kullanın
+            builder.ApplyConfiguration(new RoleConfiguration());
         }
     }
 }
